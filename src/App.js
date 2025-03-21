4ad17c1bfcef7a627ab9468d409a1e2c6a3aeda5
Kapil Hingu <kapilhingu@Kapils-MacBook-Air-2.local>
2025-03-21 18:49:31 -0500

    Updated

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to ArtGal!</h1>
+      <p>Hi</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App'; // Update the import to use uppercase App
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />); // Update to use <App />
